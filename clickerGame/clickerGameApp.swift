5040f73dc348eaf1e68c5ac0c6f404f01fcf0d5a
Anantika <43288485+3luthe@users.noreply.github.com>
2025-09-02 19:39:13 -0700

    Initial Commit

---
+++ clickerGame/clickerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickerGameApp.swift
+//  clickerGame
+//
+//  Created by Anantika Mannby on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct clickerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clickerGameTests/clickerGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  clickerGameTests.swift
+//  clickerGameTests
+//
+//  Created by Anantika Mannby on 9/2/25.
+//
+
+import Testing
+
+struct clickerGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
